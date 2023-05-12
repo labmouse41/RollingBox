fb419f79c814efe8805ec1fb7cc8cb6aca025bf1
TestSeung <sjlim589@hanmail.net>
2023-05-12 17:53:42 +0900

    레이저 변경

---
+++ Source/RythmCall/Obstacle.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Obstacle.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AObstacle::AObstacle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	MyComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	MyComp->SetSimulatePhysics(true);
+	MyComp->SetNotifyRigidBodyCollision(true);
+
+	MyComp->BodyInstance.SetCollisionProfileName("BlockAllDynamic");
+	MyComp->OnComponentHit.AddDynamic(this, &AObstacle::OnCompHit);
+
+	// Set as root component
+	RootComponent = MyComp;
+}
+
+// Called when the game starts or when spawned
+void AObstacle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AObstacle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+void AObstacle::OnCompHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL))
+	{
+		if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("I Hit: %s"), *OtherActor->GetName()));
+	}
+}
