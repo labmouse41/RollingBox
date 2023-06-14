d7d65c1dc1ac1acd00af88cc3899216eb4fbf5d8
TestSeung <sjlim589@hanmail.net>
2023-06-14 14:08:33 +0900

    WavtFloor 틀 생성

--- Source/RythmCall/WavyFloor.cpp
+++ Source/RythmCall/WavyFloor.cpp
@@ -3,6 +3,13 @@
 
 #include "WavyFloor.h"
 #include "Engine/World.h"
+#include "Components/TimelineComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/PrimitiveComponent.h" 
+#include "Materials/MaterialInstanceDynamic.h"
+#include "GameFramework/Actor.h"
 #include "TimerManager.h"
 #include "Math/Vector.h"
 #include "Math/UnrealMathUtility.h"
@@ -13,7 +20,16 @@ AWavyFloor::AWavyFloor()
 
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+    
+    WavyRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+    RootComponent = WavyRootComponent;
 
+    NeighborDetector = CreateDefaultSubobject<USphereComponent>(TEXT("Checker"));
+    NeighborDetector->InitSphereRadius(122.0f);
+    NeighborDetector->SetupAttachment(RootComponent);
+
+    StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Block"));
+    StaticMesh->SetupAttachment(RootComponent);
 }
 
 void AWavyFloor::DelayFunction(float DelayTime)
@@ -30,84 +46,131 @@ void AWavyFloor::DelayFunction(float DelayTime)
         TimerManager.SetTimer(TimerHandle_Delay, DelayTime, false);
     }
 }
-
-
-void AWavyFloor::TriggerPinsSequence(const TArray<int32>& PinOrder)
-{
-    for (int32 PinIndex : PinOrder)
-    {
-        if (PinIndex == 0)
-        {
-            // 인덱스 0번 핀에 실행해야 할 함수를 호출합니다.
-            
-        }
-        else if (PinIndex == 1)
-        {
-            // 인덱스 1번 핀에 실행해야 할 함수를 호출합니다.
-
-            // foreach loop
-        }
-
-    }
-}
-
 // Called when the game starts or when spawned
 void AWavyFloor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-    DelayFunction(BeatLength);
+	// 다음 틱에 호출될 함수입니다.
+    FTimerDelegate TimerDelegate;
+    UE_LOG(LogTemp,Warning,TEXT("BeginPlay"))
+    //Initialize();
+    TimerDelegate.BindUFunction(this, FName("Initialize"));
+    GetWorld()->GetTimerManager().SetTimer(TimerHandle, TimerDelegate, Delay, false);
 
+    if (TimelineCurve != nullptr)
+    {
 
+        FOnTimelineFloat TimeLineUpdateDelegate;
+        FOnTimelineEvent TimeLineFinishDelegate;
+
+        //Update Function 등록
+        TimeLineUpdateDelegate.BindUFunction(this, FName("TimeLineUpdateFunc"));
+        TimeLine.AddInterpFloat(TimelineCurve, TimeLineUpdateDelegate);
+
+        //Finish Function 등록
+        TimeLineFinishDelegate.BindUFunction(this, FName("TimeLineFinishFunc"));
+        TimeLine.SetTimelineFinishedFunc(TimeLineFinishDelegate);
+        
+    }
 }
-//AActor* GetActorOfClass(UObject* WorldContextObject, TSubclassOf<AActor> ActorClass)
-//{
-//    if (WorldContextObject)
-//    {
-//        UWorld* World = WorldContextObject->GetWorld();
-//        if (World)
-//        {
-//            for (TActorIterator<AActor> ActorItr(World, ActorClass); ActorItr; ++ActorItr)
-//            {
-//                return *ActorItr;
-//            }
-//        }
-//    }
-//
-//    return nullptr;
-//}
-void AWavyFloor::Trigger(bool bDelay, FVector Color, float DelayTime)
+void AWavyFloor::ServerCustomEventTrigger_Implementation(bool bDelay, float DelayTime)
 {
-	bDelay = false;
-	bTriggered = false;
-	if (!bTriggered)
-	{
-		bTriggered = true;
-        DelayFunction(bDelay * DelayTime);
-        if (bDelay)
-        {
-            AddedColor = Color;
-        }
-        else
-        {
-            FMath::SRandInit(FMath::FRand());
-            float Alpha = FMath::FRand();
+    // 서버에서 실행되는 로직을 구현합니다.
+    bDelay = false;
+    bTriggered = false;
+   
+    if (!bTriggered)
+    {
+        bTriggered = true;
 
-            FVector LerpedVector = FMath::Lerp(ColorImpact1, ColorImpact2, Alpha);
-            AddedColor = LerpedVector;
+    }
+    DelayFunction(bDelay * DelayTime);
+    MulticastCustomEventTrigger(bDelay,DelayTime);
+
+    for (const auto& Element : NeighborList)
+    {
+        DelayTime = FMath::RandRange(0.05, 0.1);
+        Element->ServerCustomEventTrigger(bDelay, DelayTime);
+    }
+}
+
+bool AWavyFloor::ServerCustomEventTrigger_Validate(bool bDelay, float DelayTime)
+{
+    // 서버에서 실행되는 커스텀 이벤트의 유효성을 검사합니다.
+    return true;
+}
+
+void AWavyFloor::MulticastCustomEventTrigger_Implementation(bool bDelay, float DelayTime)
+{
+    
+    TimeLine.PlayFromStart();
+    // 모두에서 실행되는 로직을 구현합니다.
+}
+
+void AWavyFloor::Initialize()
+{
+    NeighborDetector->GetOverlappingActors(OverlappingActors, AWavyFloor::StaticClass());
+    // 각 요소에 대한 작업 수행
+    for (auto Element : OverlappingActors)
+    {
+        AWavyFloor* WavyFloorElement = static_cast<AWavyFloor*>(Element);
+        NeighborList.Add(WavyFloorElement);
+        NeighborList.Remove(this);
+        
+        //BeatLength는 2초로 임시지정
+        SetScalarParameterValueOnMaterials(StaticMesh, FName("Initialize"), 2.0f);
+    }
+}
+void AWavyFloor::SetScalarParameterValueOnMaterials(UStaticMeshComponent* MeshComponent, FName ParameterName, float ParameterValue)
+{
+    if (MeshComponent)
+    {
+        int32 MaterialCount = MeshComponent->GetNumMaterials();
+
+        for (int32 MaterialIndex = 0; MaterialIndex < MaterialCount; ++MaterialIndex)
+        {
+            UMaterialInterface* MaterialInterface = MeshComponent->GetMaterial(MaterialIndex);
+
+            if (UMaterialInstanceDynamic* MaterialInstance = Cast<UMaterialInstanceDynamic>(MaterialInterface))
+            {
+                MaterialInstance->SetScalarParameterValue(ParameterName, ParameterValue);
+            }
+            else if (MaterialInterface)
+            {
+                UMaterialInstanceDynamic* DynamicMaterialInstance = MeshComponent->CreateDynamicMaterialInstance(MaterialIndex, MaterialInterface);
+                if (DynamicMaterialInstance)
+                {
+                    DynamicMaterialInstance->SetScalarParameterValue(ParameterName, ParameterValue);
+                }
+            }
         }
-        // 호출하고자 하는 Sequence 함수를 여기에 작성합니다.
-        TArray<int32> PinOrder;
-        PinOrder.Add(0);
-        PinOrder.Add(1);
-        TriggerPinsSequence(PinOrder);
-	}
+    }
+}
+
+
+
+void AWavyFloor::Trigger(bool bDelay, float DelayTime)
+{
+    ServerCustomEventTrigger(bDelay, DelayTime);
+}
+
+void AWavyFloor::TimeLineUpdateFunc(float Output)
+{
+    ZOffSet = (1.0f - Output) * 111.0f;
+    FVector NewLocation(this->GetActorLocation().X, this->GetActorLocation().Y, ZOffSet);
+    this->SetActorLocation(NewLocation);
+}
+
+void AWavyFloor::TimeLineFinishFunc()
+{
+    bTriggered = false;
 }
 
 // Called every frame
 void AWavyFloor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+    TimeLine.TickTimeline(DeltaTime);
 }
+
 
--- Source/RythmCall/WavyFloor.h
+++ Source/RythmCall/WavyFloor.h
@@ -4,21 +4,31 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/TimelineComponent.h"
 #include "WavyFloor.generated.h"
 
 UCLASS()
 class RYTHMCALL_API AWavyFloor : public AActor
 {
 	GENERATED_BODY()
-	
+
 public:	
 	// Sets default values for this actor's properties
 	AWavyFloor();
 
-	UFUNCTION(BlueprintCallable)
-	void Trigger(bool bDelay, const FVector Color, float DelayTime);
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly,Category = "Root")
+	class USceneComponent* WavyRootComponent;
 
-	UPROPERTY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "Map")
+	class USphereComponent* NeighborDetector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "Map")
+	class UStaticMeshComponent* StaticMesh;
+
+	UFUNCTION(BlueprintCallable)
+	void Trigger(bool bDelay, float DelayTime);
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,category = "WavyFloor")
 	bool bTriggered;
 
 	UPROPERTY()
@@ -32,6 +42,9 @@ public:
 
 	UPROPERTY()
 	TArray <AWavyFloor*> NeighborList;
+
+	UPROPERTY()
+	TArray<AActor*> OverlappingActors;
 	
 	UPROPERTY()
 	FLinearColor ColorImpact1;
@@ -42,15 +55,45 @@ public:
 	UPROPERTY()
 	FTimerHandle TimerHandle_Delay;
 
+	UPROPERTY()
+	FTimerHandle TimerHandle;
+
 	UFUNCTION()
 	void DelayFunction(float DelayTime);
 
-	UFUNCTION(Category = "Pin Sequence")
-	void TriggerPinsSequence(const TArray<int32>& PinOrder);
+	UFUNCTION()
+	void SetScalarParameterValueOnMaterials(UStaticMeshComponent* MeshComponent, FName ParameterName, float ParameterValue);
+
+	// 서버에서 실행되는 커스텀 이벤트
+	UFUNCTION(Server,BlueprintCallable,Reliable, WithValidation)
+	void ServerCustomEventTrigger(bool bDelay, float DelayTime);
+
+	// 모두에서 실행되는 커스텀 이벤트
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastCustomEventTrigger(bool bDelay, float DelayTime);
+
+	UPROPERTY(EditAnywhere)
+	class UCurveFloat* TimelineCurve;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void Initialize();
+
+	float Delay = 0.0f;
+
+	FTimeline TimeLine;
+
+	UFUNCTION()
+	void TimeLineUpdateFunc(float Output);
+
+	UFUNCTION()
+	void TimeLineFinishFunc();
+
+	UPROPERTY()
+	float ZOffSet;
 
 public:	
 	// Called every frame
